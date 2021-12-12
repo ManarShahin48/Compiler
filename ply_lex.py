2001d2d0039049b41f64a28ade7331f36862e7be
manarshahin48 <manarshahin48@gmail.com>
2021-12-12 18:06:44 +0200

    At finaaaallly, Yacc file is working ♥♥♥

--- ply_lex.py
+++ ply_lex.py
@@ -6,131 +6,135 @@ from ply.lex import TOKEN
 
 # ♦♦♦♦♦♦♦♦♦♦ Reseverd Words Definiatons ♦♦♦♦♦♦♦♦♦♦
 reseverd_words = {
-    'select': 'SELECT',
-    'from': 'FROM',
-    'into': 'INTO',
-    'where': 'WHERE',
-    'insert': 'INSERT',
-    'values': 'VALUES',
-    'delete': 'DELETE',
-    'update': 'UPDATE',
-    'set': 'SET',
-    'distinct': 'DISTINCT',
-    'count': 'COUNT',
-    'sum': 'SUM',
-    'as': 'AS',
-    'group': 'GROUP',
-    'order': 'ORDER',
-    'by': 'BY',
-    'having': 'HAVING',
-    'join': 'JOIN',
-    'inner': 'INNER',
-    'asc':  'ASC',
-    'desc': 'DESC',
-    'limit': 'LIMIT',
+    "select": "SELECT",
+    "from": "FROM",
+    "into": "INTO",
+    "where": "WHERE",
+    "insert": "INSERT",
+    "values": "VALUES",
+    "delete": "DELETE",
+    "update": "UPDATE",
+    "set": "SET",
+    "distinct": "DISTINCT",
+    "count": "COUNT",
+    "sum": "SUM",
+    "as": "AS",
+    "group": "GROUP",
+    "order": "ORDER",
+    "by": "BY",
+    "having": "HAVING",
+    "join": "JOIN",
+    "inner": "INNER",
+    "asc": "ASC",
+    "desc": "DESC",
+    "limit": "LIMIT",
 }
 
 tokens = [
-    'NUMBER',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'COLUMN_NAME',
-    'COLUMN_NUMBER',
-    'STRING',
-    'EQUAL',
-    'BIGGER_THAN_OR_EQUAL_TO',
-    'BIGGER_THAN',
-    'SMALLER_THAN_OR_EQUAL_TO',
-    'SMALLER_THAN',
-    'SIME_COLON',
-    'COMAA',
-    'LPAREN',
-    'RPAREN',
-    'SINGLE_QUOTE',
-    'DOUBLE_QUOTE',
-    'ARABIC_DOUBLE_QUOTE',
-    'R_ARABIC_DOUBLE_QUOTE',
-    'ALL',
-    'AND',
-    'OR',
-    'NOT',
-    'LBRACE',
-    'RBRACE',
-    'L_SQ_BRACE',
-    'R_SQ_BRACE',
-    'DATASOURCE',
-    'PERCENT',
-    'DOT',
+    "NUMBER",
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
+    "COLUMN_NAME",
+    "COLUMN_NUMBER",
+    "STRING",
+    "EQUAL",
+    "BIGGER_THAN_OR_EQUAL_TO",
+    "BIGGER_THAN",
+    "SMALLER_THAN_OR_EQUAL_TO",
+    "SMALLER_THAN",
+    "SIME_COLON",
+    "COMAA",
+    "LPAREN",
+    "RPAREN",
+    "SINGLE_QUOTE",
+    "DOUBLE_QUOTE",
+    "ARABIC_DOUBLE_QUOTE",
+    "R_ARABIC_DOUBLE_QUOTE",
+    "ALL",
+    "AND",
+    "OR",
+    "NOT",
+    "LBRACE",
+    "RBRACE",
+    "L_SQ_BRACE",
+    "R_SQ_BRACE",
+    "DATASOURCE",
+    "PERCENT",
+    "DOT",
 ] + list(reseverd_words.values())
 
 # Regular expression rules for simple tokens
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_EQUAL = r'='
-t_BIGGER_THAN_OR_EQUAL_TO = r'>='
-t_BIGGER_THAN = r'>'
-t_SMALLER_THAN_OR_EQUAL_TO = r'<='
-t_SMALLER_THAN = r'<'
-t_SIME_COLON = r';'
-t_COMAA = r','
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_SINGLE_QUOTE = r'\''
-t_DOUBLE_QUOTE = r'\"'
-t_ARABIC_DOUBLE_QUOTE = r'\”'
-t_R_ARABIC_DOUBLE_QUOTE = r'\“'
-t_AND = r'\&'
-t_OR = r'\|'
-t_NOT = r'\!'
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
-t_L_SQ_BRACE = r'\['
-t_R_SQ_BRACE = r'\]'
-t_DATASOURCE = r'\[[^,\]\[]+\]'
-t_COLUMN_NUMBER = r'\[\d+\]'
-t_PERCENT = r'\%'
-t_DOT = r'\.'
+t_PLUS = r"\+"
+t_MINUS = r"-"
+t_TIMES = r"\*"
+t_DIVIDE = r"/"
+t_EQUAL = r"="
+t_BIGGER_THAN_OR_EQUAL_TO = r">="
+t_BIGGER_THAN = r">"
+t_SMALLER_THAN_OR_EQUAL_TO = r"<="
+t_SMALLER_THAN = r"<"
+t_SIME_COLON = r";"
+t_COMAA = r","
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_SINGLE_QUOTE = r"\'"
+t_DOUBLE_QUOTE = r"\""
+t_ARABIC_DOUBLE_QUOTE = r"\”"
+t_R_ARABIC_DOUBLE_QUOTE = r"\“"
+t_AND = r"\&"
+t_OR = r"\|"
+t_NOT = r"\!"
+t_LBRACE = r"\{"
+t_RBRACE = r"\}"
+t_L_SQ_BRACE = r"\["
+t_R_SQ_BRACE = r"\]"
+t_DATASOURCE = r"\[[^,\]\[]+\]"
+t_COLUMN_NUMBER = r"\[\d+\]"
+t_PERCENT = r"\%"
+t_DOT = r"\."
 t_STRING = r'"([^"\n])*"'
 # A string containing ignored characters (spaces and tabs)
-t_ignore = ' \t'
-t_ignore_COMMENT = r'\#.*'
+t_ignore = " \t"
+t_ignore_COMMENT = r"\#.*"
 
-digit = r'([0-9])'
-nondigit = r'([_A-Za-z])'
+digit = r"([0-9])"
+nondigit = r"([_A-Za-z])"
 # r'[a-zA-Z_][a-zA-Z_0-9]*'
-identifier = r'(' + nondigit + r'(' + digit + r'|' + nondigit + r')*)'
-identifier = identifier + r'|' + r'\[' + digit + r'+\]'
+identifier = r"(" + nondigit + r"(" + digit + r"|" + nondigit + r")*)"
+identifier = identifier + r"|" + r"\[" + digit + r"+\]"
 
 
 def __init__(self, **kwargs):
     self.lexer = lex.lex(module=self, **kwargs)
+
 
 # Check for reserved words
 
 
 @TOKEN(identifier)
 def t_COLUMN_NAME(t):
-    t.type = reseverd_words.get(t.value, 'COLUMN_NAME')
+    t.type = reseverd_words.get(t.value, "COLUMN_NAME")
     return t
+
 
 # A regular expression rule with some action code
 
 
 def t_NUMBER(t):
-    r'\d+'
+    r"\d+"
     t.value = int(t.value)
     return t
+
 
 # Define a rule so we can track line numbers
 
 
 def t_newline(t):
-    r'\n+'
+    r"\n+"
     t.lexer.lineno += len(t.value)
+
 
 # Error handling rule
 
@@ -166,20 +170,20 @@ def t_error(t):
 
 lexer = lex.lex()
 
-if __name__ == '__main__':
-    while(True):
+if __name__ == "__main__":
+    while True:
         s = input("lex> ")
         if not s:
             break
         s = s.lower()
         lexer.input(s)
-        print('=======Tokens=======')
+        print("=======Tokens=======")
         while True:
             tok = lexer.token()
             if not tok:
                 break
             print("\t", tok.value, "\t:\t", tok.type, sep="")
-        print('====================')
+        print("====================")
 # Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 # env\Scripts\activate
--- ply_yacc.py
+++ ply_yacc.py
@@ -4,62 +4,65 @@ import ply.yacc as yacc
 import petl as etl
 import csv
 import sqlite3
+
 # Get the token map from the lexer.
 from ply_lex import tokens
 
 
-start = 'start'
+start = "start"
 
 
 def p_start(p):
-    '''start : select 
-        | insert
-        | update
-        | delete'''
+    """start : select
+    | insert
+    | update
+    | delete"""
     pass
 
 
 # ♦♦♦♦♦♦♦ OPERATIONS ♦♦♦♦♦♦♦
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
+
 def p_expression_plus(p):
-    'expression : expression PLUS term'
+    "expression : expression PLUS term"
     p[0] = p[1] + p[3]
 
 
 def p_expression_minus(p):
-    'expression : expression MINUS term'
+    "expression : expression MINUS term"
     p[0] = p[1] - p[3]
 
 
 def p_expression_term(p):
-    'expression : term'
+    "expression : term"
     p[0] = p[1]
 
 
 def p_term_times(p):
-    'term : term TIMES factor'
+    "term : term TIMES factor"
     p[0] = p[1] * p[3]
 
 
 def p_term_div(p):
-    'term : term DIVIDE factor'
+    "term : term DIVIDE factor"
     p[0] = p[1] / p[3]
 
 
 def p_term_factor(p):
-    'term : factor'
+    "term : factor"
     p[0] = p[1]
 
 
 def p_factor_num(p):
-    'factor : NUMBER'
+    "factor : NUMBER"
     p[0] = p[1]
 
 
 def p_factor_expr(p):
-    'factor : LPAREN expression RPAREN'
+    "factor : LPAREN expression RPAREN"
     p[0] = p[2]
+
 
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
@@ -67,69 +70,74 @@ def p_factor_expr(p):
 # ♦♦♦♦♦♦♦ Basic Variables ♦♦♦♦♦♦♦
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 def p_empty(p):
-    'empty :'
-    p[0] = ' '
+    "empty :"
+    p[0] = " "
 
 
 def p_op(p):
-    '''op : EQUAL
+    """op : EQUAL
     | BIGGER_THAN_OR_EQUAL_TO
     | BIGGER_THAN
     | SMALLER_THAN_OR_EQUAL_TO
-    | SMALLER_THAN'''
+    | SMALLER_THAN"""
     pass
 
 
 def p_where(p):
-    'where : WHERE condition'
+    "where : WHERE condition"
+
     p[0] = p[2]
 
 
 def p_where_empty(p):
-    'where : empty'
+    "where : empty"
     p[0] = True
 
 
 def p_condition(p):
-    'condition : expression op expression'
-    p[0] = p[1] + p[2] + p[3]
+    "condition : expression op expression"
+    pass
+
+
+# p[0] = p[1] + p[2] + p[3]
 
 
 def p_condition_parens(p):
-    'condition : LPAREN condition RPAREN'
+    "condition : LPAREN condition RPAREN"
     p[0] = p[2]
 
 
 def p_expression(p):
-    'expression : expression_term'
+    "expression : expression_term"
     p[0] = p[1]
 
 
 def p_expression_term(p):
-    'expression_term : term'
+    "expression_term : term"
     p[0] = p[1]
 
 
 def p_term(p):
-    '''term : COLUMN_NAME
+    """term : COLUMN_NAME
     | NUMBER
-    | STRING'''
+    | STRING"""
     pass
 
 
 def p_condition_and(p):
-    'condition : condition AND condition'
+    "condition : condition AND condition"
     p[0] = p[1] and p[3]
 
 
 def p_condition_or(p):
-    'condition : condition OR condition'
+    "condition : condition OR condition"
     p[0] = p[1] or p[3]
 
 
 def p_condition_not(p):
-    'condition : NOT condition'
+    "condition : NOT condition"
     p[0] = not p[2]
+
 
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
@@ -137,71 +145,80 @@ def p_condition_not(p):
 # ♦♦♦♦♦♦♦ SELECT ♦♦♦♦♦♦♦
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
+
 def p_select(p):
-    'select : SELECT column INTO DATASOURCE FROM DATASOURCE where group order limit SIME_COLON'
-    print("sssssssss")
+    """select : SELECT column into FROM DATASOURCE where group order limit SIME_COLON
+    | empty"""
+    print("Select is Done ♠")
+
+
 # PETL
-    # if p[4] == p[4].find('.db'):
-    #     with open(f'{p[4]}', 'r') as f:
-    #         connection = sqlite3.connect(f'{p[4]}')
-    #         # p[0] ==> SELECT * FROM example
-    #         table = etl.fromdb(connection, f'{p[0]}')
+# if p[4] == p[4].find('.db'):
+#     with open(f'{p[4]}', 'r') as f:
+#         connection = sqlite3.connect(f'{p[4]}')
+#         # p[0] ==> SELECT * FROM example
+#         table = etl.fromdb(connection, f'{p[0]}')
 
-    # elif p[4] == p[4].find('.csv'):
-    #     with open(f'{p[4]}', 'r') as f:
-    #         table = etl.fromcsv(f'{p[4]}')
+# elif p[4] == p[4].find('.csv'):
+#     with open(f'{p[4]}', 'r') as f:
+#         table = etl.fromcsv(f'{p[4]}')
 
-    # elif p[4] == p[4].find('.txt'):
-    #     with open(f'{p[4]}', 'r') as f:
-    #         table = etl.fromtext(f'{p[4]}')
+# elif p[4] == p[4].find('.txt'):
+#     with open(f'{p[4]}', 'r') as f:
+#         table = etl.fromtext(f'{p[4]}')
 
-    # elif p[4] == p[4].find('.xml'):
-    #     with open(f'{p[4]}', 'r') as f:
-    #         table = etl.fromxml(f'{p[4]}', 'tr', 'td')
+# elif p[4] == p[4].find('.xml'):
+#     with open(f'{p[4]}', 'r') as f:
+#         table = etl.fromxml(f'{p[4]}', 'tr', 'td')
 
-    # elif p[4] == p[4].find('.json'):
-    #     with open(f'{p[4]}', 'r') as f:
-    #         table = etl.fromjson(f'{p[4]}', header=['foo', 'bar'])
+# elif p[4] == p[4].find('.json'):
+#     with open(f'{p[4]}', 'r') as f:
+#         table = etl.fromjson(f'{p[4]}', header=['foo', 'bar'])
 
-    # elif p[4] == p[4].find('.p'):
-    #     with open(f'{p[4]}', 'r') as f:
-    #         table = etl.frompickle(f'{p[4]}')
-    # print(table)
+# elif p[4] == p[4].find('.p'):
+#     with open(f'{p[4]}', 'r') as f:
+#         table = etl.frompickle(f'{p[4]}')
+# print(table)
+
+
+def p_into(p):
+    """into : INTO DATASOURCE
+    | empty"""
 
 
 def p_group(p):
-    '''group : GROUP BY column having
-                        | empty'''
-    p[0] = ' '
+    """group : GROUP BY column having
+    | empty"""
+    pass  # p[0] = " "
 
 
 def p_having(p):
-    '''having : HAVING condition
-              | empty'''
+    """having : HAVING condition
+    | empty"""
     pass
 
 
 def p_order(p):
-    '''order : ORDER BY orders
-                    | empty'''
-    p[0] = ' '
+    """order : ORDER BY orders
+    | empty"""
+    pass  # p[0] = " "
 
 
 def p_orders(p):
-    '''orders : column way
-                    | orders COMAA orders'''
+    """orders : column way
+    | orders COMAA orders"""
     pass
 
 
 def p_way(p):
-    '''way : ASC
-                | DESC
-                | empty'''
+    """way : ASC
+    | DESC
+    | empty"""
     pass
 
 
 def p_limit(p):
-    '''limit : LIMIT NUMBER'''
+    """limit : LIMIT NUMBER"""
     if p[2] < 0:
         p[0] = None
     else:
@@ -209,7 +226,7 @@ def p_limit(p):
 
 
 def p_limit_empty(p):
-    'limit : empty'
+    "limit : empty"
     p[0] = None
 
 
@@ -218,24 +235,24 @@ def p_select_into(p):
 
 
 def p_column_all(p):
-    'column : TIMES'  # '*'
-    p[0] = ['*']
+    "column : TIMES"  # '*'
+    p[0] = ["*"]
 
 
 def p_column_name(p):
-    'column : COLUMN_NAME'
+    "column : COLUMN_NAME"
     p[0] = [p[1]]
 
 
 def p_column_number(p):
-    'column : COLUMN_NUMBER'
+    "column : COLUMN_NUMBER"
     p[1] = int(p[1][1:-1])
     p[0] = [p[1]]
 
 
 def p_columns(p):
-    '''column : column COMAA column
-                    | empty'''
+    """column : column COMAA column
+    | empty"""
     p[0] = []
     if type(p[1]) == list:
         p[0].extend(p[1])
@@ -250,42 +267,46 @@ def p_columns(p):
 # ♦♦♦♦♦♦♦ INSERT ♦♦♦♦♦♦♦
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
+
 def p_insert(p):
-    '''insert : INSERT INTO DATASOURCE icolumn VALUES LPAREN value RPAREN SIME_COLON
-                    | INSERT INTO DATASOURCE icolumn select SIME_COLON'''
-    pass
+    """insert : INSERT INTO DATASOURCE icolumn VALUES LPAREN value RPAREN SIME_COLON
+    | INSERT INTO DATASOURCE icolumn LPAREN select RPAREN SIME_COLON"""
+    print("Insert is Done ♠")
+
+
 # PETL
-    # if p[3] == p[3].find('.csv'):
-    #     table1 = [[f'{p[5]}', f'{p[5]}'],
-    #               [f'{p[9]}', f'{p[9]}'],
-    #               [f'{p[9]}', f'{p[9]}'],
-    #               [f'{p[9]}', f'{p[9]}']]
-    #     with open(f'{p[5]}', 'w') as f:
-    #         writer = csv.writer(f)
-    #         writer.writerows(table1)
-    #     table2 = etl.fromcsv('example.csv')
-    #     table2
+# if p[3] == p[3].find('.csv'):
+#     table1 = [[f'{p[5]}', f'{p[5]}'],
+#               [f'{p[9]}', f'{p[9]}'],
+#               [f'{p[9]}', f'{p[9]}'],
+#               [f'{p[9]}', f'{p[9]}']]
+#     with open(f'{p[5]}', 'w') as f:
+#         writer = csv.writer(f)
+#         writer.writerows(table1)
+#     table2 = etl.fromcsv('example.csv')
+#     table2
 
 
 def p_value_string(p):
-    'value : STRING'
+    "value : STRING"
     pass
 
 
 def p_value_number(p):
-    'value : NUMBER'
+    "value : NUMBER"
     pass
 
 
 def p_value(p):
-    'value : value COMAA value'
+    "value : value COMAA value"
     pass
 
 
 def p_icolumn(p):
-    '''icolumn : LPAREN COLUMN_NAME RPAREN
-                        | empty'''
+    """icolumn : LPAREN COLUMN_NAME RPAREN
+    | empty"""
     pass
+
 
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
@@ -293,15 +314,17 @@ def p_icolumn(p):
 # ♦♦♦♦♦♦♦ UPDATE ♦♦♦♦♦♦♦
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
+
 def p_update(p):
-    'update : UPDATE DATASOURCE SET assigns where SIME_COLON'
-    pass
+    "update : UPDATE DATASOURCE SET assigns where SIME_COLON"
+    print("Update is Done ♠")
 
 
 def p_assigns(p):
-    '''assigns : column EQUAL value
-                        | assigns COMAA assigns'''
+    """assigns : column EQUAL value
+    | assigns COMAA assigns"""
     pass
+
 
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
@@ -309,24 +332,33 @@ def p_assigns(p):
 # ♦♦♦♦♦♦♦ DELETE ♦♦♦♦♦♦♦
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
+
 def p_delete(p):
-    'delete : DELETE FROM DATASOURCE where'
-    print("sssssssss")
+    "delete : DELETE FROM DATASOURCE where SIME_COLON"
+    print("Delete is Done ♠")
+
 
 # ♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦
 
 
 # Error rule for syntax errors
-# def p_error(p):
-#     print("Syntax error in input!")
+def p_error(p):
+    print("Syntax error in input!")
 
 
 # Build the parser
 parser = yacc.yacc()
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
-        s = input('Our yacc > ')
+        s = input("The yacc :  ")
         if not s:
             continue
         result = parser.parse(s)
-        # print(result)
+
+
+# select student into [Country] from [data];
+# select student into [Country] from [data] where 3>=4 order by column desc;
+# insert into [data source] (values) values (4,3);
+# insert into [data source] (column) (select student into [Country] from [data];);
+# update [data source] set column1 = "compiler"  where 3>1;
+# delete from [data source] where 1<=7;
